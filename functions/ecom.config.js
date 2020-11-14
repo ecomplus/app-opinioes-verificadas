41a3cd8090bacba88b6146194af0755e4c5377c1
Talisson Ferreira <talissonf@gmail.com>
2020-11-13 21:48:58 -0300

    chore(ecom.config): setted title, auth_scope and procedures

--- functions/ecom.config.js
+++ functions/ecom.config.js
@@ -7,8 +7,8 @@
 
 const app = {
   app_id: 125183,
-  title: 'My Awesome E-Com Plus App',
-  slug: 'my-awesome-app',
+  title: 'Opinioes Verificadas',
+  slug: 'opinioes-verificadas',
   type: 'external',
   state: 'active',
   authentication: true,
@@ -54,82 +54,19 @@ const app = {
       'POST'           // Create procedures to receive webhooks
     ],
     products: [
-      // 'GET',           // Read products with public and private fields
+      'GET',           // Read products with public and private fields
       // 'POST',          // Create products
       // 'PATCH',         // Edit products
       // 'PUT',           // Overwrite products
       // 'DELETE',        // Delete products
     ],
-    brands: [
-      // 'GET',           // List/read brands with public and private fields
-      // 'POST',          // Create brands
-      // 'PATCH',         // Edit brands
-      // 'PUT',           // Overwrite brands
-      // 'DELETE',        // Delete brands
-    ],
-    categories: [
-      // 'GET',           // List/read categories with public and private fields
-      // 'POST',          // Create categories
-      // 'PATCH',         // Edit categories
-      // 'PUT',           // Overwrite categories
-      // 'DELETE',        // Delete categories
-    ],
-    customers: [
-      // 'GET',           // List/read customers
-      // 'POST',          // Create customers
-      // 'PATCH',         // Edit customers
-      // 'PUT',           // Overwrite customers
-      // 'DELETE',        // Delete customers
-    ],
     orders: [
-      // 'GET',           // List/read orders with public and private fields
+      'GET',           // List/read orders with public and private fields
       // 'POST',          // Create orders
       // 'PATCH',         // Edit orders
       // 'PUT',           // Overwrite orders
       // 'DELETE',        // Delete orders
-    ],
-    carts: [
-      // 'GET',           // List all carts (no auth needed to read specific cart only)
-      // 'POST',          // Create carts
-      // 'PATCH',         // Edit carts
-      // 'PUT',           // Overwrite carts
-      // 'DELETE',        // Delete carts
-    ],
-
-    /**
-     * Prefer using 'fulfillments' and 'payment_history' subresources to manipulate update order status.
-     */
-    'orders/fulfillments': [
-      // 'GET',           // List/read order fulfillment and tracking events
-      // 'POST',          // Create fulfillment event with new status
-      // 'DELETE',        // Delete fulfillment event
-    ],
-    'orders/payments_history': [
-      // 'GET',           // List/read order payments history events
-      // 'POST',          // Create payments history entry with new status
-      // 'DELETE',        // Delete payments history entry
-    ],
-
-    /**
-     * Set above 'quantity' and 'price' subresources if you don't need access for full product document.
-     * Stock and price management only.
-     */
-    'products/quantity': [
-      // 'GET',           // Read product available quantity
-      // 'PUT',           // Set product stock quantity
-    ],
-    'products/variations/quantity': [
-      // 'GET',           // Read variaton available quantity
-      // 'PUT',           // Set variation stock quantity
-    ],
-    'products/price': [
-      // 'GET',           // Read product current sale price
-      // 'PUT',           // Set product sale price
-    ],
-    'products/variations/price': [
-      // 'GET',           // Read variation current sale price
-      // 'PUT',           // Set variation sale price
-    ],
+    ]
 
     /**
      * You can also set any other valid resource/subresource combination.
@@ -145,55 +82,16 @@ const app = {
 
 const procedures = []
 
-/**
- * Uncomment and edit code above to configure `triggers` and receive respective `webhooks`:
-
 const { baseUri } = require('./__env')
 
 procedures.push({
   title: app.title,
 
   triggers: [
-    // Receive notifications when new order is created:
-    {
-      resource: 'orders',
-      action: 'create',
-    },
-
-    // Receive notifications when order financial/fulfillment status changes:
-    {
-      resource: 'orders',
-      field: 'financial_status',
-    },
     {
       resource: 'orders',
       field: 'fulfillment_status',
-    },
-
-    // Receive notifications when products/variations stock quantity changes:
-    {
-      resource: 'products',
-      field: 'quantity',
-    },
-    {
-      resource: 'products',
-      subresource: 'variations',
-      field: 'quantity'
-    },
-
-    // Receive notifications when cart is edited:
-    {
-      resource: 'carts',
-      action: 'change',
-    },
-
-    // Receive notifications when customer is deleted:
-    {
-      resource: 'customers',
-      action: 'delete',
-    },
-
-    // Feel free to create custom combinations with any Store API resource, subresource, action and field.
+    }
   ],
 
   webhooks: [
@@ -207,9 +105,6 @@ procedures.push({
     }
   ]
 })
-
- * You may also edit `routes/ecom/webhook.js` to treat notifications properly.
- */
 
 exports.app = app
 
